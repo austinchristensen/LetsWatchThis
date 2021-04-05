2811627bf348c60c654bde4a1dc4b0f749efd383
Austin <austinchristensen4@gmail.com>
2021-04-05 16:54:45 -0600

    Initial Commit

---
+++ LetsWatchThis/LetsWatchThisApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsWatchThisApp.swift
+//  LetsWatchThis
+//
+//  Created by Austin Christensen on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct LetsWatchThisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
